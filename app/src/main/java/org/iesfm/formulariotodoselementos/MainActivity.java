f4e438782a64cdb10180b86938dc15508bf6ba88
Miguel <mufrikiler@gmail.com>
2021-11-17 23:47:44 +0100

    Hacer que funcionen las selecciones de los spinners

--- app/src/main/java/org/iesfm/formulariotodoselementos/MainActivity.java
+++ app/src/main/java/org/iesfm/formulariotodoselementos/MainActivity.java
@@ -3,16 +3,21 @@ package org.iesfm.formulariotodoselementos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     protected static final String[] contactos = {"Selecciona", "Móvil", "Email"};
     protected static final String[] deportes = {"Selecciona", "Futbol", "Baloncesto"};
+
+    private TextView tvMovil;
+    private TextView tvEmail;
 
     private EditText etNombre;
     private EditText etApellidos;
@@ -34,7 +39,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         iniciarVariables();
-        crearSpinerContactos();
+        crearSpinnerContactos();
+        crearSpinnerDeportes();
+
+
     }
 
     public void iniciarVariables() {
@@ -43,6 +51,13 @@ public class MainActivity extends AppCompatActivity {
         etApellidos = (EditText) findViewById(R.id.etApellido);
         etMovil = (EditText) findViewById(R.id.etPhone);
         etEmail = (EditText) findViewById(R.id.etEmail);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
+        tvMovil = (TextView) findViewById(R.id.tvMovil);
+
+        etEmail.setVisibility(View.GONE);
+        etMovil.setVisibility(View.GONE);
+        tvMovil.setVisibility(View.GONE);
+        tvEmail.setVisibility(View.GONE);
 
         contenedor = (RadioGroup) findViewById(R.id.contenedor);
         posicion1 = (RadioButton) findViewById(R.id.posicion1);
@@ -52,14 +67,57 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void crearSpinerContactos(){
+    public void crearSpinnerContactos() {
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,contactos);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, contactos);
         sContactos = (Spinner) findViewById(R.id.sContacto);
-
         sContactos.setAdapter(adapter);
+
+        switch (sContactos.getSelectedItemPosition()){
+            case 1:
+                etMovil.setVisibility(View.VISIBLE);
+                tvMovil.setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                etEmail.setVisibility(View.VISIBLE);
+                tvEmail.setVisibility(View.VISIBLE);
+                break;
+        }
+
     }
 
+
+    public void crearSpinnerDeportes() {
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, deportes);
+        sDeportes = (Spinner) findViewById(R.id.sDeporte);
+        sDeportes.setAdapter(adapter);
+
+        int seleccion = sDeportes.getSelectedItemPosition();
+
+        switch (sDeportes.getSelectedItem().toString()){
+            case "Futbol":
+                posicion1.setText("Portero");
+                posicion2.setText("defensa");
+                posicion3.setText("lateral");
+                posicion4.setText("Extremo");
+                break;
+            case "Baloncesto":
+                posicion1.setText("Pivot");
+                posicion2.setText("Base");
+                posicion3.setText("Entrenador");
+                posicion4.setText("Utillero");
+        }
+
+        if (seleccion == 1){
+            posicion1.setText("Portero");
+            posicion2.setText("defensa");
+            posicion3.setText("lateral");
+            posicion4.setText("Extremo");
+
+
+        }
+
+    }
 
 
 }
